0e0d017d81ff3a1a4322e00287310bc21d5b7936
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-03-19 01:08:34 +0900

    Add framework project

---
+++ SysInfo/SysInfo.h
@@ -0,0 +1,19 @@
+//
+//  SysInfo.h
+//  SysInfo
+//
+//  Created by Daisuke T on 2019/03/18.
+//  Copyright © 2019 SysInfo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SysInfo.
+FOUNDATION_EXPORT double SysInfoVersionNumber;
+
+//! Project version string for SysInfo.
+FOUNDATION_EXPORT const unsigned char SysInfoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SysInfo/PublicHeader.h>
+
+
