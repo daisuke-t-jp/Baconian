2397ede557cb23a97c8a907d8757e59073734177
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-03-19 01:26:01 +0900

    Add macOS demo

---
+++ demo/SysInfoDemo-macOS/SysInfoDemo-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SysInfoDemo-macOS
+//
+//  Created by Daisuke T on 2019/03/18.
+//  Copyright © 2019 SysInfoDemo-macOS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ demo/SysInfoDemo-macOS/SysInfoDemo-macOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SysInfoDemo-macOS
+//
+//  Created by Daisuke T on 2019/03/18.
+//  Copyright © 2019 SysInfoDemo-macOS. All rights reserved.
+//
+
+import Cocoa
+import SysInfo
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
