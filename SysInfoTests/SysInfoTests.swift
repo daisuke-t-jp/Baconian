62c9fb1ae41b435e2bce6ac4c8f5cb1e024384a2
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-03-19 01:12:27 +0900

    Update Unittest

--- SysInfoTests/SysInfoTests.swift
+++ SysInfoTests/SysInfoTests.swift
@@ -19,16 +19,55 @@ class SysInfoTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMachHostStatics() {
+		let val = Mach.hostStatics()
+		XCTAssertNotEqual(val.freeSize, 0)
+		XCTAssertNotEqual(val.activeSize, 0)
+		XCTAssertNotEqual(val.inactiveSize, 0)
+		XCTAssertNotEqual(val.wireSize, 0)
     }
 
+	func testMachTaskInfo() {
+		let val = Mach.taskInfo()
+		XCTAssertNotEqual(val.virtualSize, 0)
+		XCTAssertNotEqual(val.residentSize, 0)
+		XCTAssertNotEqual(val.residentSizeMax, 0)
+	}
+	
+	func testMachThreadInfo() {
+		let array = Mach.threadInfoArray()
+		XCTAssert(array.count > 0)
+	}
+	
+	func testOSMemoryInfo() {
+		let val = SysInfoReporter.osMemoryInfo()
+		XCTAssertNotEqual(val.freeSize, 0)
+		XCTAssertNotEqual(val.activeSize, 0)
+		XCTAssertNotEqual(val.inactiveSize, 0)
+		XCTAssertNotEqual(val.wireSize, 0)
+		XCTAssertNotEqual(val.totalSize, 0)
+		XCTAssertNotEqual(val.usedSize, 0)
+		XCTAssertNotEqual(val.unusedSize, 0)
+	}
+	
+	func testProcessMemoryInfo() {
+		let val = SysInfoReporter.processMemoryInfo()
+		XCTAssertNotEqual(val.residentSize, 0)
+		XCTAssertNotEqual(val.residentSizeMax, 0)
+	}
+	
+	func testThreadInfo() {
+		let val = SysInfoReporter.threadInfo()
+		XCTAssertNotEqual(val.num, 0)
+	}
+	
+	/*
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
+	*/
 
 }
