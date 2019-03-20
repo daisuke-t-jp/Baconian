a07de3cdab775b1a42f70b79ce6b9ec76f7df5d6
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-03-20 23:29:27 +0900

    Update prototypes

--- prototypes/SysInfoPrototype/SysInfoPrototype/main.swift
+++ prototypes/SysInfoPrototype/SysInfoPrototype/main.swift
@@ -8,56 +8,82 @@
 
 import Foundation
 
-while(true) {
-	autoreleasepool {
-		print("# Test")
+class dummy: ReporterDelegate {
+	func reporter(_ manager: Reporter, didUpdate data: Reporter.Data) {
+		print("# Reporter")
 		print("- \(Date())")
-		print("")
-
-		do {
-			print("## Mach")
-			
-			print("### Host")
-			print("#### VMStatics")
-			print("- \(Mach.Host.vmStatics())")
-			print("#### CPULoadInfo")
-			print("- \(Mach.Host.cpuLoadInfo())")
-			print("#### ProcessorInfo")
-			print("- \(Mach.Host.processorInfo())")
-			print("")
-			
-			print("### Task")
-			print("#### BasicInfo")
-			print("- \(Mach.Task.basicInfo())")
-			print("#### ThreadBasicInfo")
-			print("- \(Mach.Task.threadBasicInfo())")
-			print("")
-		}
-		
-		do {
-			print("## Report")
-			
-			print("### OS")
-			print("#### Memory")
-			print("- \(Report.OS.memory())")
-			print("#### CPU")
-			print("- \(Report.OS.cpu())")
-			print("#### Processors")
-			print("- \(Report.OS.processors())")
-			print("")
-			
-			print("### Process")
-			print("#### Memory")
-			print("- \(Report.Process.memory())")
-			print("#### CPU")
-			print("- \(Report.Process.cpu())")
-			print("#### Thread")
-			print("- \(Report.Process.thread())")
-			print("")
-		}
-		
+		print("- \(data)")
 		print("----------")
-		
-		Thread.sleep(forTimeInterval: 1)
 	}
+}
+
+
+func testImmediate() {
+	print("# Immediate")
+	print("- \(Date())")
+	print("")
+	
+	print("## Mach")
+	print("### Host")
+	print("#### VMStatics")
+	print("- \(Mach.Host.vmStatics())")
+	print("#### CPULoadInfo")
+	print("- \(Mach.Host.cpuLoadInfo())")
+	print("#### ProcessorInfo")
+	print("- \(Mach.Host.processorInfo())")
+	print("")
+	print("### Task")
+	print("#### BasicInfo")
+	print("- \(Mach.Task.basicInfo())")
+	print("#### ThreadBasicInfo")
+	print("- \(Mach.Task.threadBasicInfo())")
+	print("")
+	
+	print("## Report")
+	print("### OS")
+	print("#### Memory")
+	print("- \(Report.OS.memory())")
+	print("#### CPU")
+	print("- \(Report.OS.cpu())")
+	print("#### Processors")
+	print("- \(Report.OS.processors())")
+	print("")
+	print("### Process")
+	print("#### Memory")
+	print("- \(Report.Process.memory())")
+	print("#### CPU")
+	print("- \(Report.Process.cpu())")
+	print("#### Thread")
+	print("- \(Report.Process.thread())")
+	print("")
+	print("----------")
+}
+
+
+let enableImmediate = false
+let obj = dummy()
+
+if !enableImmediate {
+	Reporter.sharedManager.delegate = obj
+	Reporter.sharedManager.updateFrequency = .veryOften
+	Reporter.sharedManager.start()
+}
+
+
+while(true) {
+	
+	autoreleasepool {
+		
+		if enableImmediate {
+			testImmediate()
+		
+			Thread.sleep(forTimeInterval: 1)
+		} else {
+			RunLoop.current.run()
+			
+			Thread.sleep(forTimeInterval: 0.1)
+		}
+		
+	}
+	
 }
