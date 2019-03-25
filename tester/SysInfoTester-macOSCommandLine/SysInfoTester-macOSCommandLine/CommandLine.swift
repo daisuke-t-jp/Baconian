8a1e233b9000f98de3b56fe06f4ceb9ca1d9b787
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-03-25 20:26:41 +0900

    Add CommandLine class

---
+++ tester/SysInfoTester-macOSCommandLine/SysInfoTester-macOSCommandLine/CommandLine.swift
@@ -0,0 +1,182 @@
+//
+//  CommandLine.swift
+//  SysInfoTester-macOSCommandLine
+//
+//  Created by Daisuke T on 2019/03/25.
+//  Copyright © 2019 SysInfoTester-macOSCommandLine. All rights reserved.
+//
+
+import Foundation
+
+// Commands:
+// -reporter start				start report.
+// -reporter stop				stop report.
+// -reporter delegateEnable		delegate switch to enable.
+// -reporter delegateDisable	delegate switch to disable.
+// -reporter frequency [normally|often|veryoften]	set frequency.
+// -reporter data				show last update data.
+// -test memoryAlloc			allocate test memory.
+// -test memoryDealloc			deallocate test memory.
+// -test threadCreate			create test thread.
+// -test threadDestroy			destroy test thread.
+class CommandLine {
+	
+	// MARk: Enum, Const
+	private static let commandTypeReporter = "-reporter"
+	private static let commandTypeTest = "-test"
+
+	private static let commandValueReporterStart = "start"
+	private static let commandValueReporterStop = "stop"
+	private static let commandValueReporterDelegateEnable = "delegateEnable"
+	private static let commandValueReporterDelegateDisable = "delegateDisable"
+	private static let commandValueReporterFrequency = "frequency"
+	private static let commandValueReporterData = "data"
+	
+	private static let commandValueReporterFrequencyNormally = "normally"
+	private static let commandValueReporterFrequencyOften = "often"
+	private static let commandValueReporterFrequencyVeryOften = "veryOften"
+	
+	private static let commandValueTestMemoryAlloc = "memoryAlloc"
+	private static let commandValueTestMemoryDealloc = "memoryDealloc"
+	private static let commandValueTestThreadCreate = "threadCreate"
+	private static let commandValueTestThreadDestroy = "threadDestroy"
+	
+	
+	// MARK: Property
+	private let tester = Tester()
+
+	
+	func run(_ data: Data) {
+		var str = String(data: data, encoding: String.Encoding.utf8) ?? ""
+		str = str.trimmingCharacters(in: NSCharacterSet.newlines)
+		let array = str.components(separatedBy: " ")
+		guard array.count > 0 else {
+			return
+		}
+		
+		let command = array[0].lowercased()
+		if command == CommandLine.commandTypeReporter.lowercased() {
+			commandReporter(array)
+			return
+		} else if command == CommandLine.commandTypeTest.lowercased() {
+			commandTest(array)
+			return
+		}
+	}
+	
+	
+	// MARK: Command Report
+	func commandReporter(_ array: [String]) {	// swiftlint:disable:this cyclomatic_complexity
+		guard array.count >= 2 else {
+			return
+		}
+		
+		let value = array[1].lowercased()
+		if value == CommandLine.commandValueReporterStart.lowercased() {
+			commandReporterStart()
+		} else if value == CommandLine.commandValueReporterStop.lowercased() {
+			commandReporterStop()
+		} else if value == CommandLine.commandValueReporterDelegateEnable.lowercased() {
+			commandReporterDelegateEnable()
+		} else if value == CommandLine.commandValueReporterDelegateDisable.lowercased() {
+			commandReporterDelegateDisable()
+		} else if value == CommandLine.commandValueReporterStart.lowercased() {
+			guard array.count >= 3 else {
+				return
+			}
+			
+			let value2 = array[2].lowercased()
+			if value2 == CommandLine.commandValueReporterFrequencyNormally.lowercased() {
+				commandReporterFrequency(.normally)
+			} else if value2 == CommandLine.commandValueReporterFrequencyOften.lowercased() {
+				commandReporterFrequency(.often)
+			} else if value2 == CommandLine.commandValueReporterFrequencyVeryOften.lowercased() {
+				commandReporterFrequency(.veryOften)
+			}
+		} else if value == CommandLine.commandValueReporterData.lowercased() {
+			commandReporterData()
+		}
+	}
+	
+	func commandReporterStart() {
+		print("Reporter start")
+		tester.reporterStart()
+	}
+	
+	func commandReporterStop() {
+		print("Reporter stop")
+		tester.reporterStop()
+	}
+	
+	func commandReporterDelegateEnable() {
+		print("Delegate enable")
+		tester.reporterDelegateEnable()
+	}
+	
+	func commandReporterDelegateDisable() {
+		print("Delegate disable")
+		tester.reporterDelegateDisable()
+	}
+	
+	func commandReporterFrequency(_ frequency: Reporter.Frequency) {
+		print("Frequency \(frequency)")
+		tester.reporterSetFrequency(frequency)
+	}
+	
+	func commandReporterData() {
+		print("Data")
+		print("# OS")
+		print("## Memory")
+		print(tester.data.osMemory)
+		print("## CPU")
+		print(tester.data.osCPU)
+		print("## Processors")
+		print(tester.data.osProcessors)
+		print("# Process")
+		print("## Memory")
+		print(tester.data.processMemory)
+		print("## CPU")
+		print(tester.data.processCPU)
+		print("## Thread")
+		print(tester.data.processThread)
+	}
+	
+	
+	// MARK: Command Test
+	func commandTest(_ array: [String]) {
+		guard array.count >= 2 else {
+			return
+		}
+		
+		let value = array[1].lowercased()
+		if value == CommandLine.commandValueTestMemoryAlloc.lowercased() {
+			commandTestMemoryAlloc()
+		} else if value == CommandLine.commandValueTestMemoryDealloc.lowercased() {
+			commandTestMemoryDealloc()
+		} else if value == CommandLine.commandValueTestThreadCreate.lowercased() {
+			commandTestThreadCreate()
+		} else if value == CommandLine.commandValueTestThreadDestroy.lowercased() {
+			commandTestThreadDestroy()
+		}
+	}
+	
+	func commandTestMemoryAlloc() {
+		print("Memory alloc")
+		tester.memoryAlloc(1024 * 1024 * 32)
+	}
+
+	func commandTestMemoryDealloc() {
+		print("Memory dealloc")
+		tester.memoryDealloc()
+	}
+	
+	func commandTestThreadCreate() {
+		print("Thread create")
+		tester.threadCreate(1024 * 32, sleepInterval: 0.01)
+	}
+	
+	func commandTestThreadDestroy() {
+		print("Thread destroy")
+		tester.threadDestroy()
+	}
+}
--- tester/SysInfoTester-macOSCommandLine/SysInfoTester-macOSCommandLine/main.swift
+++ tester/SysInfoTester-macOSCommandLine/SysInfoTester-macOSCommandLine/main.swift
@@ -8,110 +8,14 @@
 
 import Foundation
 
-let tester = Tester()
+let commandLine = CommandLine()
 
-let commandTypeStart = "-start"
-let commandTypeStop = "-stop"
-let commandTypeDelegateEnable = "-delegateEnable"
-let commandTypeDelegateDisable = "-delegateDisable"
-let commandTypeFrequency = "-frequency"
-let commandTypeData = "-data"
-let commandTypeTest = "-test"
-
-let commandValueFrequencyNormally = "normally"
-let commandValueFrequencyOften = "often"
-let commandValueFrequencyVeryOften = "veryOften"
-
-let commandValueTestMemoryAlloc = "memoryAlloc"
-let commandValueTestMemoryDealloc = "memoryDealloc"
-let commandValueTestThreadCreate = "threadCreate"
-let commandValueTestThreadDestroy = "threadDestroy"
-// Commands:
-// -start				start report.
-// -stop				stop report.
-// -delegateEnable		delegate switch to enable.
-// -delegateDisable		delegate switch to disable.
-// -frequency [normally|often|veryoften]	set frequency.
-// -data				show last update data.
-// -test memoryAlloc	allocate test memory.
-// -test memoryDealloc	deallocate test memory.
-// -test threadCreate	create test thread.
-// -test threadDestroy	destroy test thread.
 while true {
 	
 	autoreleasepool {
-		var inputStr = String(data: FileHandle.standardInput.availableData,
-							  encoding: String.Encoding.utf8) ?? ""
-		inputStr = inputStr.trimmingCharacters(in: NSCharacterSet.newlines)
-		let array = inputStr.components(separatedBy: " ")
-		guard array.count > 0 else {
-			return
-		}
+		let data = FileHandle.standardInput.availableData
 		
-		if array[0].lowercased() == commandTypeStart.lowercased() {
-			print("Reporter start")
-			tester.reporterStart()
-		} else if array[0].lowercased() == commandTypeStop.lowercased() {
-			print("Reporter stop")
-			tester.reporterStop()
-		} else if array[0].lowercased() == commandTypeDelegateEnable.lowercased() {
-			print("Delegate enable")
-			tester.reporterDelegateEnable()
-		} else if array[0].lowercased() == commandTypeDelegateDisable.lowercased() {
-			print("Delegate disable")
-			tester.reporterDelegateDisable()
-		} else if array[0].lowercased() == commandTypeFrequency.lowercased() {
-			guard array.count >= 2 else {
-				return
-			}
-			
-			if array[1].lowercased() == commandValueFrequencyNormally.lowercased() {
-				print("Frequency normally")
-				tester.reporterSetFrequency(.normally)
-			} else if array[1].lowercased() == commandValueFrequencyOften.lowercased() {
-				print("Frequency often")
-				tester.reporterSetFrequency(.often)
-			} else if array[1].lowercased() == commandValueFrequencyVeryOften.lowercased() {
-				print("Frequency veryoften")
-				tester.reporterSetFrequency(.veryOften)
-			}
-		} else if array[0].lowercased() == commandTypeData.lowercased() {
-			print("Data")
-			print("# OS")
-			print("## Memory")
-			print(tester.data.osMemory)
-			print("## CPU")
-			print(tester.data.osCPU)
-			print("## Processors")
-			print(tester.data.osProcessors)
-			print("# Process")
-			print("## Memory")
-			print(tester.data.processMemory)
-			print("## CPU")
-			print(tester.data.processCPU)
-			print("## Thread")
-			print(tester.data.processThread)
-		}
-		
-		if array[0].lowercased() == commandTypeTest.lowercased() {
-			guard array.count >= 2 else {
-				return
-			}
-			
-			if array[1].lowercased() == commandValueTestMemoryAlloc.lowercased() {
-				print("Memory alloc")
-				tester.memoryAlloc(1024 * 1024 * 32)
-			} else if array[1].lowercased() == commandValueTestMemoryDealloc.lowercased() {
-				print("Memory dealloc")
-				tester.memoryDealloc()
-			} else if array[1].lowercased() == commandValueTestThreadCreate.lowercased() {
-				print("Thread create")
-				tester.threadCreate(1024 * 32, sleepInterval: 0.01)
-			} else if array[1].lowercased() == commandValueTestThreadDestroy.lowercased() {
-				print("Thread destroy")
-				tester.threadDestroy()
-			}
-		}
+		commandLine.run(data)
 	}
 	
 }
