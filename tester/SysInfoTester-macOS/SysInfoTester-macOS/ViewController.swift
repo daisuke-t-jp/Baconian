ebb54d2743940a734c61181788d9ffae749959f9
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-03-27 20:47:45 +0900

    Add tester/macOS

---
+++ tester/SysInfoTester-macOS/SysInfoTester-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SysInfoTester-macOS
+//
+//  Created by Daisuke T on 2019/03/27.
+//  Copyright © 2019 SysInfoTester-macOS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
