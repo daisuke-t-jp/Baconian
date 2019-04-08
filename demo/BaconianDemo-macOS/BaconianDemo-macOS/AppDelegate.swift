1b244bf85a61b78f2ecb423b23f6d6c65291686d
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-04-08 21:28:55 +0900

    Add empty macOS demo project

---
+++ demo/BaconianDemo-macOS/BaconianDemo-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BaconianDemo-macOS
+//
+//  Created by Daisuke T on 2019/04/08.
+//  Copyright © 2019 BaconianDemo-macOS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ demo/BaconianDemo-macOS/BaconianDemo-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BaconianDemo-macOS
+//
+//  Created by Daisuke T on 2019/04/08.
+//  Copyright © 2019 BaconianDemo-macOS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
